2a4f0a79395821193733bf53671302c9d839ac0e
Yuri Trondin <lopata@2ch.hk>
2024-09-21 22:11:45 +0500

    база header и footer

--- frontend/app/components/layout/Footer/Footer.tsx
+++ frontend/app/components/layout/Footer/Footer.tsx
@@ -4,8 +4,20 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <p>
-        &copy; {new Date().getFullYear()} Tea Timer App. All rights reserved.
+        &copy; {new Date().getFullYear()} Чайный таймер.
       </p>
+      <ul className={styles.footerLinkContainer}>
+      <li>
+        <a href="/about" className={styles.footerLink}>
+          О проекте
+        </a>
+      </li>
+      <li>
+        <a href="/contacts" className={styles.footerLink}>
+          Контакты
+        </a>
+      </li>
+      </ul>
     </footer>
   );
 };
--- frontend/app/components/layout/Header/Header.tsx
+++ frontend/app/components/layout/Header/Header.tsx
@@ -4,21 +4,27 @@ const Header = () => (
   <header className={styles.header}>
     <ul className={styles.menuList}>
       <li>
-        <a href="#" className={styles.menuItem}>
-          Home
+        <a href="/tea-list" className={styles.menuItem}>
+          Каталог
         </a>
       </li>
       <li>
-        <a href="#" className={styles.menuItem}>
-          About
+        <a href="/login" className={styles.menuItem}>
+          Логин
         </a>
       </li>
       <li>
-        <a href="#" className={styles.menuItem}>
-          Contact
+        <a href="/about" className={styles.menuItem}>
+          О проекте
         </a>
       </li>
     </ul>
+    <div>
+      <a href="/user" className={styles.menuItem}>
+        Профиль
+      </a>
+      <button>Выход</button>
+    </div>
   </header>
 );
 
